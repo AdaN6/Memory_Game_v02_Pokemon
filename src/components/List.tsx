68fb3893cb8de03818b4c72f754266093c0e9c7f
AdaN6 <a_disney_da@hotmail.com>
2023-01-04 00:51:51 +0100

    testing people map function

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -9,10 +9,15 @@ interface IProps {
 }
 
 
-const List: React.FC<IProps> = ({people}) => {
+const List: React.FC<IProps> = ({ people }) => {
   return (
     <div>
-      ho
+     <ul>
+        {people.map(person => {
+            return (
+            <div>{person.name}</div>)
+        })}
+     </ul>
     </div>
   )
 }
