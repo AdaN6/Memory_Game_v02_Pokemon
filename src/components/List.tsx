579cde280980bb2a399d7c29cb2cbb233cfd566a
AdaN6 <a_disney_da@hotmail.com>
2023-01-04 01:04:51 +0100

    added JSX.Element and return the jsx in the renderList function

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -11,9 +11,10 @@ interface IProps {
 
 const List: React.FC<IProps> = ({ people }) => {
   
-    const renderList = () => {
+// added JSX.Element to define what to return
+    const renderList = (): JSX.Element[] => {
         return people.map((person) => {
-            <li className="List">
+            return (<li className="List">
                 <div className="List-header">
                     <img className="List-img" src={person.url} />
                     <h2>{person.name}</h2>
@@ -21,6 +22,7 @@ const List: React.FC<IProps> = ({ people }) => {
                 <p> {person.age} years old</p>
                 <p className="List-note">{person.note}</p>
             </li>
+            )
         })
     }
   
